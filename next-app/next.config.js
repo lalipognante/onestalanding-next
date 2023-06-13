6254c67504d202a9c9948d598c254d3dc11b45e6
laura.pognante <laura.pognante@proveedor.ues21.edu.ar>
2023-06-13 17:30:25 -0300

    add email logic

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -3,18 +3,7 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-  },
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-
-    return config;
-  },
-  resolve: {
-    fallback: {
-      net: false
-    }
-  },
+  }
 };
 
 module.exports = nextConfig
--- next-app/sections/Subscribe.jsx
+++ next-app/sections/Subscribe.jsx
@@ -1,24 +1,56 @@
 'use client';
+import React, { useState } from 'react';
 
-import React, { useState } from 'react'
 export default function Subscribe() {
   const [isSubscribed, setIsSubscribed] = useState(false);
+  const [email, setEmail] = useState('');
 
-  const handleButtonClick = () => {
-    setIsSubscribed(!isSubscribed);
+  async function handleButtonClick() {
+    if (email.trim() === '') {
+      return; // No se permite enviar un correo electrónico vacío
+    }
+
+    try {
+      // Realiza una solicitud POST a la API de contact
+      const response = await fetch('/api/contact', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      if (response.ok) {
+        setIsSubscribed(true);
+      } else {
+        console.error('Error al enviar el correo electrónico');
+      }
+    } catch (error) {
+      console.error('Error de red:', error);
+    }
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
   };
 
   return (
     <div className="subscribe-container">
       <div className="subscribe-content">
         <form className={`subscription ${isSubscribed ? 'done' : ''}`}>
-          <input className="add-email" type="email" placeholder="subscribe@me.now" />
-            <button className="submit-email" type="button" onClick={handleButtonClick}>
-              <span className="before-submit">Subscribe</span>
-              <span className="after-submit">Thank you for subscribing!</span>
-            </button>
+          <input
+            className="add-email"
+            type="email"
+            placeholder="subscribe@me.now"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <button className="submit-email" type="button" onClick={handleButtonClick}>
+            <span className="before-submit">Subscribe</span>
+            <span className="after-submit">Thank you for subscribing!</span>
+          </button>
         </form>
       </div>
     </div>
-  )
+  );
 }
