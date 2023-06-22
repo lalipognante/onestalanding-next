c0e0bb371345cd7615a077be98f1fbad4ab1368d
laura.pognante <laura.pognante@proveedor.ues21.edu.ar>
2023-06-22 17:02:20 -0300

    add real images and real description

--- next-app/sections/TeamMembers.jsx
+++ next-app/sections/TeamMembers.jsx
@@ -9,18 +9,40 @@ export const TeamMembers = () => {
       name: 'Gabriel Pognante',
       position: 'Especialista en Compliance',
       // image: icon1
+      curriculum: [
+        "Abogado (U.C.C)",
+        "Especialista en Derecho Penal Económico (U.B.P)",
+        "Diplomado en Crimen Organizado, Corrupción y Terrorismo (Univ. De Salamanca, España)",
+        "Programa en Compliance y Prevención de la Corrupción Corporativa (Univ. S21)",
+        "Certificación Internacional en Ética y Compliance (AAEC)"
+      ]
     },
     {
       id: 2,
-      name: 'Ray Marie',
-      position: 'Developer',
+      name: 'Federico Alfaro',
+      position: 'Abogado',
       // icon: icon2
+      curriculum: [
+        "Abogado (U.B.A)",
+        "Maestrando en Derecho Empresario",
+        "MBA en Gestión y Dirección de Organizaciones (U.B.P)",
+        "Diplomado en Métodos Alternativos de Resolución de Conflictos",
+        "Con más de 10 años de trayectoria como abogado independiente en el área civil, comercial y empresarial."
+      ]
     },
     {
       id: 3,
-      name: 'Austin Min',
-      position: 'Designer',
+      name: 'Maria Elisa Jaramillo',
+      position: 'Abogada',
       // icon: icon3
+      curriculum: [
+        "Abogada (UHE)",
+        "Maestrando en Derecho de Propiedad Intelectual y Nuevas Tecnologías",
+        "Diplomada en Propiedad Intelectual y en Derechos Humanos",
+        "Más de 4 años de trayectoria como abogada en Ecuador en las áreas corporativa e inmobiliario",
+        "2 años como abogada en Argentina en las áreas civil y empresarial",
+        "Certificada con nivel B2 de inglés (Cambridge)"
+      ]
     }
   ]
   return (
